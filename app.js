ece42c4ff3367d4caf50397718d99482a697cbc2
achrefmechergui <achreffcb123achref@gamil.com>
2025-01-11 21:56:53 +0100

    done

---
+++ app.js
@@ -0,0 +1,112 @@
+// var day = "wedenesday"  ;  //// asign
+// day = "friday"  //// reasign
+
+
+// with else , else if , else  ::: 
+// const day = "thursday" ;
+
+// if (day === "monday") {
+//     console.log("plan course structure")
+//     console.log("go to coding meetup")
+// }
+
+// else if (day === "tuesday"){
+//   console.log("prepare nice exercices")
+// }
+
+// else if (day === "wedenesday" || day === "thursday") {
+//     console.log("write and test code")
+// }
+
+// else if (day === "friday" || day === "saturday" || day === "sunday"){
+//     console.log("enjoy the weekend !!! ")
+// }
+// else {
+//     console.log("not a valid day !!! ")
+// }
+
+
+
+/////////////////////////////////////////////////////////////////////////////////////
+
+
+///////////////// with switch , case :::  
+
+// const day = "monday" ;
+// case ==> hala ..
+
+// switch (day) {  
+//     case  "monday" : 
+//     console.log("plan course structure")
+//     console.log("go to coding meetup")
+//  break
+//     case "tuesday" : 
+//     console.log("prepare nice exercices")
+//     break
+//     case "wedenesday" :
+//     case "thursday" :
+//     console.log("write and test code")
+//     break
+//     case "friday" :   
+//     case "saturday" :
+//     case "sunday" :
+//     console.log("enjoy the weekend !!! ")
+//     break
+//     default :
+//     console.log("not a valid day !!! ")
+
+
+// }
+
+////////////////////////////////////////////////////////////////////////
+
+//// statement and expressions :: 
+
+/// expression : 
+// let x = 5 ;
+
+//// statement : 
+
+// if (true){
+//  console.log("ok")
+// }
+
+// const firstName = "nader"
+
+// console.log("my name is nader")
+
+// console.log("my name is"+" "+ firstName);
+
+// console.log(`my name is ${firstName}`)
+// console.log( if (true){
+//       console.log("ok")
+//      })
+
+
+
+//// the conditional using ternary operator (3 parts , 3 parties) :
+
+
+// normal if statement :: 
+// const age = 20 ; 
+
+// if (age >= 18) {
+//     console.log("he can get a driver license 🎉")
+// }
+// else {
+//     console.log("no he still a kid 👦")
+// }
+
+
+
+// const message = age >=18 ? "he can get a driver license 🎉" : "no he still a kid 👦"
+
+// console.log("message:" , message)
+
+
+// syntax =====>condition ? true : false
+
+//////////////////////////////////////////////////////////////////////////
+let age = 20 ;
+
+console.log(`i'm ${age} years old so ${age>=18 ? "i can get a driver license 🎉" : "i still a kid 👦"} `)
